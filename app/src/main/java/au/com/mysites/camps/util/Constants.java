005ed528993c3b8aa3ef1ca8d8557fc12d47760b
kirk <vonkratzmann@gmail.com>
2018-09-06 17:39:58 +1000

    Testing

--- app/src/main/java/au/com/mysites/camps/util/Constants.java
+++ app/src/main/java/au/com/mysites/camps/util/Constants.java
@@ -1,11 +1,8 @@
 package au.com.mysites.camps.util;
 
 public final class Constants {
-
-
+    
     private static final String PACKAGE_NAME = "au.com.mysites.camps";
-
-
 
     // Storage Permissions request code
     public static final int RC_EXTERNAL_STORAGE = 10;
@@ -42,7 +39,4 @@ public final class Constants {
     public static final String RECEIVER = PACKAGE_NAME + ".RECEIVER";
     public static final String RESULT_DATA_KEY = PACKAGE_NAME + ".RESULT_DATA_KEY";
     public static final String LOCATION_DATA_EXTRA = PACKAGE_NAME + ".LOCATION_DATA_EXTRA";
-
-
-
 }
